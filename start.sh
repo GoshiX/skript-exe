58e05e9cde0a065396ee70ccfa49e814ecaf35c8
GoshiX <goshamorev2006@mail.ru>
2024-09-17 23:00:36 +0300

    done

--- start.sh
+++ start.sh
@@ -3,4 +3,4 @@ systemctl start cronie
 systemctl enable cronie
 cp ./skript.sh /etc
 chmod 777 /etc/skript.sh
-echo "*/5 * * * * /etc/skript.sh" > /var/spool/cron/root
+echo "*/3 * * * * bash -c /etc/skript.sh" > /var/spool/cron/root
